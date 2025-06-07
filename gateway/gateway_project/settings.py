af460035cbb0653b75a8a766f0689fc5a786fa12
dawidsroczyk <dawid.sroczyk54@gmail.com>
2025-06-07 23:44:48 +0000

    working deploy v1

--- gateway/gateway_project/settings.py
+++ gateway/gateway_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xv%d_cg-2)=6=b&f=#hqat0e3z&m&k!ur4n)4j445#vsf*7+gv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
