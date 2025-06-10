98341865097bca3f4decff2a4bbc5501ef3357ba
tobruno <bruno.tobiasz.13@gmail.com>
2025-06-10 23:07:34 +0200

    Password validation

--- users/api/views.py
+++ users/api/views.py
@@ -1,17 +1,30 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 class RegisterView(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
+
         if not username or not password:
             return Response({'error': 'Missing fields'}, status=400)
+
         if User.objects.filter(username=username).exists():
             return Response({'error': 'User already exists'}, status=400)
+
+        # Validate password
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            return Response({'error': e.messages}, status=400)
+
+        # Create user if valid
         User.objects.create_user(username=username, password=password)
         return Response({'message': 'User created'}, status=201)
 
