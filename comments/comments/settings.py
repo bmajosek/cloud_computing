99e5c451092d525801e68bc5e9a1100dfc9e87d3
tobruno <bruno.tobiasz.13@gmail.com>
2025-06-10 20:20:33 +0200

    DB definition change for comments

--- comments/comments/settings.py
+++ comments/comments/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'comments.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,15 +89,15 @@ DATABASES = {
             'connect_timeout': 5,
         }
     }
-}
-'''
+}'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 
 # Password validation
